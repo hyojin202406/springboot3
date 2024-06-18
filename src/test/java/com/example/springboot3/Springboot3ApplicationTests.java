1b85ade50c58dba0d0e76c24323bafd3e6052861
hyojin202406 <hyojin202406@gmail.com>
2024-06-18 20:45:05 +0900

    [공통] Querydsl 설정

--- src/test/java/com/example/springboot3/Springboot3ApplicationTests.java
+++ src/test/java/com/example/springboot3/Springboot3ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.springboot3;
 
+import com.example.springboot3.domain.QUser;
+import com.example.springboot3.domain.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class Springboot3ApplicationTests {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Test
-    void contextLoads() {
+    void contestLoads() {
+        User user = new User("test@gmail.com", "test", "name");
+        em.persist(user);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QUser qUser = new QUser("u");
+
+        User result = query.selectFrom(qUser).fetchOne();
+
+        assertThat(result).isEqualTo(user);
+        assertThat(result.getEmail()).isEqualTo(user.getEmail());
     }
 
 }
